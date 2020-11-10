3a6234f0772bb48c23aa6b3a76541675cb0eb8b8
JR Lherm <lherm.jr@gmail.com>
2020-11-10 20:54:13 +0100

    add kata

--- 6kyu/6kyu-Duplicate_Encoder/kata.rb
+++ 6kyu/6kyu-Duplicate_Encoder/kata.rb
@@ -12,10 +12,10 @@ end
 
 # Better solution
 
-def duplicate_encode(word)
-  word
-      .downcase
-      .chars
-      .map { |char| word.downcase.count(char) > 1 ? letter = ')' : letter = '(' }
-      .join
-end
+# def duplicate_encode(word)
+#   word
+#       .downcase
+#       .chars
+#       .map { |char| word.downcase.count(char) > 1 ? letter = ')' : letter = '(' }
+#       .join
+# end
