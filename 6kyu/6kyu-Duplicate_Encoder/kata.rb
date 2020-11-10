e20c361f2156a4869d16ab37fc8b559cc8842cce
JR Lherm <lherm.jr@gmail.com>
2020-11-10 20:53:36 +0100

    add kata

---
+++ 6kyu/6kyu-Duplicate_Encoder/kata.rb
@@ -0,0 +1,21 @@
+def duplicate_encode(word)
+
+  word.downcase!
+  splitted_word = word.split("")
+
+  counts = Hash.new(0)
+  splitted_word.each { |element| counts[element] += 1 }
+
+  splitted_word.collect! {|x| counts[x] > 1 ? ")" : "("}.join
+
+end
+
+# Better solution
+
+def duplicate_encode(word)
+  word
+      .downcase
+      .chars
+      .map { |char| word.downcase.count(char) > 1 ? letter = ')' : letter = '(' }
+      .join
+end
